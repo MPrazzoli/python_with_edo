5827e69067a30a5a2343618551c02d11eb24a47e
MPrazzoli <52362087+MPrazzoli@users.noreply.github.com>
2021-01-14 15:34:43 +0100

    [CORRELATION-PICKLE]

--- pickle_obj/prova_pickle.py
+++ pickle_obj/prova_pickle.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 def main():
     project_root = os.path.dirname(os.path.dirname(__file__))
-    df = pd.read_pickle(project_root + '/pickle_obj/american_stock_dataframes/A.pkl')
+    df = pd.read_pickle(project_root + '/pickle_obj/stock_dataframes/A.pkl')
     print(project_root)
     print(df)
     df1 = df.loc['2020-07-16':'2021-01-12']
