99d1e44c66f1b2b8b8d1ec3248ca395cbc413c28
MPrazzoli <52362087+MPrazzoli@users.noreply.github.com>
2021-01-14 14:38:09 +0100

    ambiente

---
+++ pickle_obj/prova_pickle.py
@@ -0,0 +1,14 @@
+# Imported packages
+import os
+import pandas as pd
+
+
+def main():
+    project_root = os.path.dirname(os.path.dirname(__file__))
+    df = pd.read_pickle(project_root + '/pickle_obj/american_stock_dataframes/A.pkl')
+    print(project_root)
+    print(df)
+    df1 = df.loc['2020-07-16':'2021-01-12']
+
+if __name__ == "__main__":
+    main()
