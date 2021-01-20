d1006197b2a3159628363b9b9e04efb67f0542b0
MPrazzoli <52362087+MPrazzoli@users.noreply.github.com>
2021-01-20 17:43:55 +0100

    [PROVA-PICKLE][RENAME-VARIABLES]

--- pickle_obj/prova_pickle.py
+++ pickle_obj/prova_pickle.py
@@ -5,10 +5,10 @@ import pandas as pd
 
 def main():
     project_root = os.path.dirname(os.path.dirname(__file__))
-    df = pd.read_pickle(project_root + '/pickle_obj/stock_dataframes/AGC.pkl')
+    all_data_df = pd.read_pickle(project_root + '/pickle_obj/stock_dataframes/AAPL.pkl')
     print(project_root)
-    print(df)
-    df1 = df.loc['2020-07-19':'2021-01-15']
+    print(all_data_df)
+    df_filtered = all_data_df.loc['2020-07-19':'2021-01-15']
 
 if __name__ == "__main__":
     main()
