3fdb835ce57c80ecc9cfc7d7f9e9ccfc86b3e315
MPrazzoli <52362087+MPrazzoli@users.noreply.github.com>
2021-01-21 10:11:43 +0100

    [20210120]

--- pickle_obj/prova_pickle.py
+++ pickle_obj/prova_pickle.py
@@ -8,7 +8,7 @@ def main():
     all_data_df = pd.read_pickle(project_root + '/pickle_obj/stock_dataframes/AAPL.pkl')
     print(project_root)
     print(all_data_df)
-    df_filtered = all_data_df.loc['2020-07-19':'2021-01-15']
+    # df_filtered = all_data_df.loc['2020-07-19':'2021-01-15']
 
 if __name__ == "__main__":
     main()
