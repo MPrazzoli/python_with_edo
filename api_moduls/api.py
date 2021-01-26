06b2af5b1fd1089720573d57e3181d130159c921
MPrazzoli <52362087+MPrazzoli@users.noreply.github.com>
2021-01-26 15:00:36 +0100

    [COMMENTS]

--- api_moduls/api.py
+++ api_moduls/api.py
@@ -43,7 +43,7 @@ def api_function(start, end, not_found_tickers, daily_app):
     # Getting the list of all tickers we are interested in from the ticker_isin_file.xlsx excel file,
     # then a StockListClass instance is initialize to store the list of tickers and the list
     # of isin which we insert in our excel file
-    ticker_python = pd.read_excel(data_path + '/ticker_isin_file.xlsx', sheet_name='American')
+    ticker_python = pd.read_excel(data_path + '/ticker_isin_file.xlsx', sheet_name='American') # new-to-update-period
     ticker_python.set_index('ticker', inplace=True)  # Setting the index of ticker_python DataFrame with Tickers columns
 
     ticker_python.drop(not_found_tickers, inplace=True)
